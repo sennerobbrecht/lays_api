f3dc51bcf8e2eccba12a81d2106e612bd58e570c
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-17 20:04:49 +0100

    feat: user mappen structuur

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ app.get('/api/v1', (req, res) => {
 });
 
 const bagRoutes = require('./routes/bag.routes');
+const userRoutes = require('./routes/user.routes');
+
 app.use('/api/v1/bag', bagRoutes);
+app.use('/api/v1/user', userRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
