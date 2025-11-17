a117d8edaa0370896ed417370f43da4ef00b77a6
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-17 20:19:42 +0100

    feat: get api/v1/user/id gebruiker in detal kunnen bekijken en niet ze allemaal

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const users = require('../data/users');
 
-
+// POST /api/v1/user
 exports.createUser = (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
@@ -29,4 +29,19 @@ exports.createUser = (req, res) => {
   const { password: _, ...safeUser } = newUser;
 
   res.status(201).json(safeUser);
+};
+
+// GET /api/v1/user/:id
+exports.getUserById = (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = users.find(u => u.id === id);
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+ 
+  const { password, ...safeUser } = user;
+
+  res.json(safeUser);
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { createUser } = require('../controllers/user.controller');
+const { createUser, getUserById } = require('../controllers/user.controller');
 
 router.post('/', createUser);
+
+router.get('/:id', getUserById);
 
 module.exports = router;
