6dc0f4265ddf62156ce172c32442cdde644760ad
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-17 17:48:26 +0100

    feat: api deel 1: get api/v1/bags

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ const app = express();
 
 app.use(express.json());
 
+const bags = [
+  {
+    id: 1,
+    name: 'Spicy Cheese Explosion',
+    image: 'spicy-cheese.png',
+    bagColor: '#ffcc00',
+    font: 'Comic Sans',
+    pattern: 'stripes',
+    packaging: 'plastic',
+    inspiration: 'movie night',
+    keyFlavours: ['cheese', 'chili'],
+    user: 'test-user@example.com'
+  }
+];
 
 app.get('/api/v1', (req, res) => {
   res.send('Lays API v1 is running');
+});
+
+app.get('/api/v1/bag', (req, res) => {
+  res.json(bags);
 });
 
 const PORT = process.env.PORT || 4000;
