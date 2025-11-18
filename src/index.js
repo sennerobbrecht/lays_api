80fbc481dc99b509caa21c81a62861b641289239
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-18 13:32:56 +0100

    feat: update bug

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ app.use(express.json());
 
 connectDB();
 
-const bagRoutes = require('./routes/bag.routes');
-app.use('/api/v1/bag', bagRoutes);
 
-const userRoutes = require('./routes/user.routes');
-app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/bag', require('./routes/bag.routes'));
+
+
+app.use('/api/v1/user', require('./routes/user.routes'));
+
 
 app.use('/api/v1/vote', require('./routes/vote.routes'));
-
 
 app.get('/', (req, res) => {
   res.send('Lays API v1 running');
