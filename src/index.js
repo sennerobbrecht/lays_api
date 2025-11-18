9dc596e7628d8c9a1433a84c6706ebb2ad7c51ec
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-18 12:51:45 +0100

    feat: fixing package.json

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-const connectDB = require('./config/db');
-connectDB();
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const connectDB = require('./config/db');
 
+const app = express();
 app.use(express.json());
 
-app.get('/api/v1', (req, res) => {
-  res.send('Lays API v1 is running');
+// Connect MongoDB
+connectDB();
+
+// Routes
+app.use('/api/v1/bag', require('./routes/bag.routes'));
+
+app.get('/', (req, res) => {
+  res.send('Lays API v1 running');
 });
-
-const bagRoutes = require('./routes/bag.routes');
-const userRoutes = require('./routes/user.routes');
-const voteRoutes = require('./routes/vote.routes');
-
-app.use('/api/v1/bag', bagRoutes);
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/vote', voteRoutes);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`API running on http://localhost:${PORT}/api/v1`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
