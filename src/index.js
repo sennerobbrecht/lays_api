53a263e0f2f31286d0fa2cb6187751d83777ce39
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-19 15:22:09 +0100

    feat: cors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); 
 const connectDB = require('./config/db');
 
 const app = express();
+
+
+app.use(cors({
+  origin: '*',               
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 app.use(express.json());
 
 connectDB();
 
-
 app.use('/api/v1/bag', require('./routes/bag.routes'));
-
-
 app.use('/api/v1/user', require('./routes/user.routes'));
-
-
 app.use('/api/v1/vote', require('./routes/vote.routes'));
 
 app.get('/', (req, res) => {
