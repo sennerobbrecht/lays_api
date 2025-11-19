9d485fe795f2f59c4765d699000bbb6513147426
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-11-19 15:11:47 +0100

    feat: bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,20 +2,21 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
 exports.register = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
-  
+   
     const exists = await User.findOne({ email });
     if (exists) return res.status(400).json({ error: 'Email already in use' });
 
    
     const hashedPassword = await bcrypt.hash(password, 10);
 
-  
+
     const user = await User.create({
+      firstName,
+      lastName,
       email,
       password: hashedPassword
     });
@@ -24,6 +25,8 @@ exports.register = async (req, res) => {
       message: 'User registered',
       user: {
         id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email
       }
     });
@@ -31,6 +34,7 @@ exports.register = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
 
 
 exports.login = async (req, res) => {
